56e3d79aa915ad5ae14ccff34558297345b75621
Patrícia da Silva Ramos <56210981+PatriciaRamosS@users.noreply.github.com>
2022-09-06 11:41:03 -0300

    Update slip.py

--- slip.py
+++ slip.py
@@ -1,3 +1,8 @@
+# GRUPO
+# Patrícia da Silva Ramos
+# Marcelina Maye Abaga Maye
+
+
 class CamadaEnlace:
     ignore_checksum = False
 
@@ -40,9 +45,11 @@ class CamadaEnlace:
 
 
 class Enlace:
+
     def __init__(self, linha_serial):
         self.linha_serial = linha_serial
         self.linha_serial.registrar_recebedor(self.__raw_recv)
+        self.buffer = b''
 
     def registrar_recebedor(self, callback):
         self.callback = callback
@@ -51,7 +58,10 @@ class Enlace:
         # TODO: Preencha aqui com o código para enviar o datagrama pela linha
         # serial, fazendo corretamente a delimitação de quadros e o escape de
         # sequências especiais, de acordo com o protocolo CamadaEnlace (RFC 1055).
-        pass
+        datagrama = datagrama.replace(b'\xDB', b'\xDB\xDD')
+        datagrama = datagrama.replace(b'\xC0', b'\xDB\xDC')
+
+        self.linha_serial.enviar(b'\xC0' + datagrama + b'\xC0')
 
     def __raw_recv(self, dados):
         # TODO: Preencha aqui com o código para receber dados da linha serial.
@@ -61,4 +71,18 @@ class Enlace:
         # vir quebrado de várias formas diferentes - por exemplo, podem vir
         # apenas pedaços de um quadro, ou um pedaço de quadro seguido de um
         # pedaço de outro, ou vários quadros de uma vez só.
-        pass
+        dadosRecebidos = dados.split(b'\xC0')
+
+        while dadosRecebidos:
+            self.buffer += dadosRecebidos.pop(0)
+
+            if len(dadosRecebidos) > 0 and len(self.buffer) > 0:
+                self.buffer = self.buffer.replace(b'\xDB\xDC',
+                                                  b'\xC0').replace(
+                                                      b'\xDB\xDD', b'\xDB')
+                try:
+                    self.callback(self.buffer)
+                except:
+                    pass
+                finally:
+                    self.buffer = b''
